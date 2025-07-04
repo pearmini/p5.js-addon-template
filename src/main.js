4eb38b0a7d904d0ae66bcf34c3923c5297ae704f
Bairui Su <pearminipro@gmail.com>
2025-07-04 15:25:22 -0400

    Add class example

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ export function addonTemplate(p5, fn, lifecycles){
   fn.myAddonVariable = 'some value';     // This variable will be available in a sketch as `myAddonVariable`
   fn.drawSomething = function(){};       // This function will be available in the sketch as `drawSomething`
   fn.loadSomething = async function(){}; // This is an asynchronous function: for example, `await loadSomething()`
+  p5.MyAddonClass = class {};            // This class will be available in a sketch as `new p5.MyAddonClass()`
 };
 
 if(typeof p5 !== 'undefined'){
